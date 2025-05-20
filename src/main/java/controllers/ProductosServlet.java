c634f670af1afa4cdfd6a562ff793f3647964237
ESTUDIANTE <estebanasp8@gmail.com>
2025-05-20 11:50:35 -0500

    Thrid Commit

--- src/main/java/controllers/ProductosServlet.java
+++ src/main/java/controllers/ProductosServlet.java
@@ -23,9 +23,9 @@ public class ProductosServlet extends HttpServlet {
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws IOException , ServletException {
         LoginService session = new LoginServiceSessionImplement();
         Optional<String> usernameOptional = session.getUsername(req);
-        if  (usernameOptional.isPresent()) {
         ProductoService service = new ProductoServiceImplement();
         List<Productos> productos = service.listar();
+        boolean mostrarP = usernameOptional.isPresent();
         resp.setContentType("text/html;charset=UTF-8");
         PrintWriter out=resp.getWriter();
         out.println("<!DOCTYPE html>");
@@ -36,29 +36,30 @@ public class ProductosServlet extends HttpServlet {
         out.println("</head>");
         out.println("<body>");
         out.println("<h1>Listar Productos</h1>");
-        out.println("<h2>Hola "+ usernameOptional.get()+ ", Bienvenido!</h2>");
+        if (mostrarP==true){
+        out.println("<h2>Hola "+ usernameOptional.get()+ ", Bienvenido!</h2>");}
         out.println("<table>");
         out.println("<tr>");
         out.println("<th> ID PRODUCTO </th>");
         out.println("<th> NOMBRE </th>");
         out.println("<th> TIPO </th>");
-        out.println("<th> PRECIO </th>");
+        if(mostrarP==true){
+        out.println("<th> PRECIO </th>");}
         out.println("</tr>");
         productos.forEach(p-> {
             out.println("<tr>");
             out.println("<td>"+p.getId()+"</td>");
             out.println("<td>"+p.getNombre()+"</td>");
             out.println("<td>"+p.getTipo()+"</td>");
-            out.println("<td>"+p.getPrecio()+"</td>");
+            if(mostrarP==true){
+                out.println("<td>"+p.getPrecio()+"</td>");
+            }
             out.println("</tr>");
         });
         out.println("</table>");
         out.println("</body>");
         out.println("</html>");
 
-        } else {
-
-        }
     }
 }
 
